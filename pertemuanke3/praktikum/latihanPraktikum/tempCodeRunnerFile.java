f7d13eb4fc2888b5d99f76de91845fd9ea5bd310
GabrielBatavia <gabrielbatavia7@gmail.com>
2024-03-02 18:53:57 +0700

    modify laporanke3

---
+++ pertemuanke3/praktikum/latihanPraktikum/tempCodeRunnerFile.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class latihan2_mahasiswa {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Mahasiswa[] daftarMahasiswa = new Mahasiswa[3]; //kita memiliki 3 mahasiswa
+        
+        for (int i = 0; i < daftarMahasiswa.length; i++) {
+            System.out.println("Masukkan detail untuk mahasiswa ke-" + (i + 1) + ":");
+            System.out.print("Masukkan Nama: ");
+            String nama = scanner.nextLine();
+            System.out.print("Masukkan NIM: ");
+            String nim = scanner.nextLine();
+            System.out.print("Masukkan Jenis kelamin (L/P): ");
+            char jenisKelamin = scanner.nextLine().charAt(0);
+            System.out.print("Masukkan IPK: ");
+            double ipk = scanner.nextDouble();
+            scanner.nextLine(); 
+            
+            daftarMahasiswa[i] = new Mahasiswa(nama, nim, jenisKelamin, ipk);
+        }
+
+        System.out.println("\nInformasi Mahasiswa:");
+        for (Mahasiswa mahasiswa : daftarMahasiswa) {
+            System.out.println(mahasiswa.dapatkanInformasi());
+        }
+        
+        scanner.close();
+    }
+}
+
+class Mahasiswa {
+    private String nama;
+    private String nim;
+    private char jenisKelamin;
+    private double ipk;
+
+    public Mahasiswa(String nama, String nim, char jenisKelamin, double ipk) {
+        this.nama = nama;
+        this.nim = nim;
+        this.jenisKelamin = jenisKelamin;
+        this.ipk = ipk;
+    }
+
+    public String dapatkanInformasi() {
+        return "Nama: " + nama + "\nNIM: " + nim + "\nJenis kelamin: " + (jenisKelamin == 'L' ? "Laki-laki" : "Perempuan") + "\nIPK: " + ipk + "\n";
+    }
+}
