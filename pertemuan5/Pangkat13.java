71f70c881c6c817e99bb8a2f0ec893b258de7d87
GabrielBatavia <gabrielbatavia7@gmail.com>
2024-03-13 11:48:13 +0700

    modify pangkat13

--- pertemuan5/Pangkat13.java
+++ pertemuan5/Pangkat13.java
@@ -1,3 +1,14 @@
 public class Pangkat13 {
     
+    public int nilai, pangkat;
+
+    int pangkatBF(int a, int n) {
+        int hasil = 0;
+        for (int i = 0; i < n; i++) {
+            hasil *= a;
+        }
+        return hasil;
+    }
+
+
 }
