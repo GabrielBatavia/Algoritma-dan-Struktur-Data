cd2c6eaa20076a1de7bc4c6fcbd147a798f3057d
GabrielBatavia <gabrielbatavia7@gmail.com>
2024-03-13 11:23:08 +0700

    add and start pertemuan 5

---
+++ pertemuan4/manusia.java
@@ -0,0 +1,13 @@
+class manusia {
+    float Bmi;
+
+
+public manusia(int beratBdn, int tinggiBdn){
+    float Bmi = beratBdn / (tinggiBdn)*2;
+    }   
+
+    public class void main ( []args ){
+        int beratBdn = 160
+    }
+
+}
---
+++ pertemuan4/mobil.java
@@ -0,0 +1,23 @@
+public class mobil {
+    
+    public int kecepatan;
+
+    public static void main(String[] args) {
+
+    int s, t;
+        
+	mobil[] mobil1 = new mobil[3];
+
+    }
+
+    int hitungkecepatan(int s, int t) {
+        this.s = s;
+        this.t = t;
+        kecepatan = s/t;
+    }
+
+    for (int i = 0; i <mobil.length; i++) {
+        mobil1[i] = new mobil();
+    }
+
+}
