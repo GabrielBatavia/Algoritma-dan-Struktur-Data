aebed0954d7b3c1b46a1dcfaf9ffe3022af27efb
GabrielBatavia <gabrielbatavia7@gmail.com>
2024-03-13 13:06:51 +0700

    fixed erorr in dc pangkat13 and spread the main in pangkat13

--- pertemuan5/Pangkat13.java
+++ pertemuan5/Pangkat13.java
@@ -1,5 +1,3 @@
-import java.util.Scanner;
-
 public class Pangkat13 {
     
     public int nilai, pangkat;
@@ -14,7 +12,7 @@ public class Pangkat13 {
 
     int pangkatDC(int a, int n) {
         if(n==1) {
-            return 1;
+            return a;
         } else {
             if(n % 2 == 1) {
                 return (pangkatDC(a,n/2)*pangkatDC(a,n/2)*a);
@@ -23,42 +21,4 @@ public class Pangkat13 {
             }
         }
     }
-
-    public static void main(String[] args) {
-        
-        Scanner sc13 = new Scanner(System.in);
-        System.out.println("==================================================");
-        System.out.println("Masukkan jumlah elemen yang dihitung: ");
-        int elemen = sc13.nextInt();
-
-        Pangkat13[] png = new Pangkat13[elemen];
-        for(int i=0; i < elemen; i++) {
-            png[i] = new Pangkat13();
-            System.out.println("Masukkan nilai yang hendak dipangkatkan: ");
-            png[i].nilai = sc13.nextInt();
-            System.out.println("Masukkan nilai pemangkat: ");
-            png[i].pangkat = sc13.nextInt();
-        }
-
-        System.out.println("Hasil Pangkat - Brute Force");
-        for(int i=0; i < elemen; i++) {
-            System.out.println("Hasil dari "
-                + png[i].nilai+ " pangkat "
-                + png[i].pangkat+ " adalah "
-                + png[i].pangkatBF(png[i].nilai, png[i].pangkat)
-            );
-        }
-        System.out.println("Hasil Pangkat - Devide And Conquer");
-        for(int i=0; i < elemen; i++) {
-            System.out.println("Hasil dari "
-                + png[i].nilai+ " pangkat "
-                + png[i].pangkat+ " adalah "
-                + png[i].pangkatDC(png[i].nilai, png[i].pangkat)
-            );
-        }
-    }
-
-
-
-
 }
---
+++ pertemuan5/Pangkat13_main.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class Pangkat13_main {
+        public static void main(String[] args) {
+        
+        Scanner sc13 = new Scanner(System.in);
+        System.out.println("==================================================");
+        System.out.println("Masukkan jumlah elemen yang dihitung: ");
+        int elemen = sc13.nextInt();
+
+        Pangkat13[] png = new Pangkat13[elemen];
+        for(int i=0; i < elemen; i++) {
+            png[i] = new Pangkat13();
+            System.out.println("Masukkan nilai yang hendak dipangkatkan: ");
+            png[i].nilai = sc13.nextInt();
+            System.out.println("Masukkan nilai pemangkat: ");
+            png[i].pangkat = sc13.nextInt();
+        }
+
+        System.out.println("Hasil Pangkat - Brute Force");
+        for(int i=0; i < elemen; i++) {
+            System.out.println("Hasil dari "
+                + png[i].nilai+ " pangkat "
+                + png[i].pangkat+ " adalah "
+                + png[i].pangkatBF(png[i].nilai, png[i].pangkat)
+            );
+        }
+        System.out.println("Hasil Pangkat - Devide And Conquer");
+        for(int i=0; i < elemen; i++) {
+            System.out.println("Hasil dari "
+                + png[i].nilai+ " pangkat "
+                + png[i].pangkat+ " adalah "
+                + png[i].pangkatDC(png[i].nilai, png[i].pangkat)
+            );
+        }
+    }
+
+}
