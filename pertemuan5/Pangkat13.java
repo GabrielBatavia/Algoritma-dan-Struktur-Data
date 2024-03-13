b528382f08f0a776f87a8a925bcf3ecb4b2d12da
GabrielBatavia <gabrielbatavia7@gmail.com>
2024-03-13 12:31:31 +0700

    modify pangkat13

--- pertemuan5/Pangkat13.java
+++ pertemuan5/Pangkat13.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Pangkat13 {
     
     public int nilai, pangkat;
@@ -21,6 +23,42 @@ public class Pangkat13 {
             }
         } 
     }
+
+    public static void main(String[] args) {
+        
+        Scanner sc13 = new Scanner(System.in);
+        System.out.println("==================================================");
+        System.out.println("Masukkan jumlah elemen yang dihitung: ");
+        int elemen = sc13.nextInt();
+
+        Pangkat13[] png = new Pangkat13[elemen];
+        for(int i=0; i < elemen; i++) {
+            png[i] = new Pangkat13();
+            System.out.println("Masukkan nilai yang hendak dipangkatkan: ");
+            int nilai = sc13.nextInt();
+            System.out.println("Masukkan nilai pemangkat: ");
+            int pengkat = sc13.nextInt();
+        }
+
+        System.out.println("Hasil Pangkat - Brute Force");
+        for(int i=0; i < elemen; i++) {
+            System.out.println("Hasil dari "
+                + png[i].nilai+ " pangkat "
+                + png[i].pangkat+ " adalah "
+                + png[i].pangkatDC(png[i].nilai, png[i].pangkat)
+            );
+        }
+        System.out.println("Hasil Pangkat - Devide And Conquer");
+        for(int i=0; i < elemen; i++) {
+            System.out.println("Hasil dari "
+                + png[i].nilai+ " pangkat "
+                + png[i].pangkat+ " adalah "
+                + png[i].pangkatDC(png[i].nilai, png[i].pangkat)
+            );
+        }
+    }
+
+
 
 
 }
