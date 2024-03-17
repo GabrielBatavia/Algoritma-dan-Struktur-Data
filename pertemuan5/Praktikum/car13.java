932c9ab26f2cfca1ae5bfc2feceeb8990ee12d37
GabrielBatavia <gabrielbatavia7@gmail.com>
2024-03-17 19:07:49 +0700

    start latihan1 and add and modify car13

---
+++ pertemuan5/Praktikum/car13.java
@@ -0,0 +1,12 @@
+public class car13 {
+    String merk, tipe;
+    int tahun, top_acceleration,top_power;
+
+    public car13(String merk, String tipe, int tahun, int top_acceleration, int top_power) {
+        this.merk = merk;
+        this.tipe = tipe;
+        this.tahun = tahun;
+        int top_acc = top_acceleration;
+        int top_pw = top_power;
+    }
+}
