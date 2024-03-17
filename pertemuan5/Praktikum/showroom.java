c5fd168d11f0d887d72d18d3cb22ac9362828d54
GabrielBatavia <gabrielbatavia7@gmail.com>
2024-03-17 19:25:02 +0700

    modify showroom and instance carArray13 and the atribut

--- pertemuan5/Praktikum/showroom.java
+++ pertemuan5/Praktikum/showroom.java
@@ -1,20 +1,14 @@
 public class showroom {
     
-    private car13[] cars;
-    private int count;
+    car13[] carArray13 = new car13[7]; 
 
-    public showroom(int capacity) {
-        cars = new car13[capacity];
-        count = 0;
-    }
-
-    public void addCar(car13 car) {
-        if (count < cars.length) {
-            cars[count] = car;
-            count++;
-        } else {
-            System.out.println("Showroom Sudah Full, tidak bisa menambahkan modil lagi");
-        }
-    }
+    carArray13[0] = new Car13("BMW", "M2 Coupe", 2016, 6816, 728);
+    carArray13[1] = new Car13("Ford", "Fiesta ST", 2014, 3921, 575);
+    carArray13[2] = new Car13("Nissan", "370Z", 2009, 4360, 657);
+    carArray13[3] = new Car13("Subaru", "BRZ", 2014, 4058, 609);
+    carArray13[4] = new Car13("Subaru", "Impreza WRX STI", 2013, 6255, 703);
+    carArray13[5] = new Car13("Toyota", "AE86 Trueno", 1986, 3700, 553);
+    carArray13[6] = new Car13("Toyota", "86/GT86", 2014, 4180, 609);
+    carArray13[7] = new Car13("Toyota", "Golf GTI", 2014, 4180, 631);
 
 }
