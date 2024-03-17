9719c61513556b7ad4c40945f55170fa0ba0894b
GabrielBatavia <gabrielbatavia7@gmail.com>
2024-03-17 19:17:28 +0700

    modify showroom and make cars array

--- pertemuan5/Praktikum/showroom.java
+++ pertemuan5/Praktikum/showroom.java
@@ -1,3 +1,20 @@
 public class showroom {
     
+    private car13[] cars;
+    private int count;
+
+    public showroom(int capacity) {
+        cars = new car13[capacity];
+        count = 0;
+    }
+
+    public void addCar(car13 car) {
+        if (count < cars.length) {
+            cars[count] = car;
+            count++;
+        } else {
+            System.out.println("Showroom Sudah Full, tidak bisa menambahkan modil lagi");
+        }
+    }
+
 }
