780ee39b51ead0d19935ce621d34d86b4c586de6
GabrielBatavia <gabrielbatavia7@gmail.com>
2024-03-27 11:23:36 +0700

    fixing error in BukuMain13

--- pertemuan7/BukuMain13.java
+++ pertemuan7/BukuMain13.java
@@ -4,32 +4,36 @@ import java.util.Scanner;
 
 public class BukuMain13 {
     
-    Scanner s = new Scanner(System.in);
-    Scanner s1 = new Scanner(System.in);
-
-    PencarianBuku13 data = new PencarianBuku13();
-    int jumBuku = 5;
-
-    System.out.println("================================================");
-    System.out.println("Masukkan data Buku secara Urut dari kodeBuku terkecil : ");
-    for (int i = 0; i < jumBuku; i++) {
-        System.out.println("Kode Buku \t: ");
-        int kodeBuku = s.nextInt();
-        System.out.println("Judul Buku \t: ");
-        String judulBuku = s1.nextLine();
-        System.out.println("Tahun terbit \t: ");
-        int tahunTerbit = s.nextInt();
-        System.out.println("Pengarang \t: ");
-        String Pengarang = s1.nextLine();
-        System.out.println("Stock \t: ");
-        int stock = s.nextInt();
-
-        Buku13 m = new Buku13(kodeBuku, judulBuku, tahunTerbit, stock, Pengarang);
-        data.tambah(m);
-    }
-
-    System.out.println("================================================");
-    System.out.println("Data keseluruhan Mahasiswa : ");
+    public static void main(String[] args) {
     
 
+        Scanner s = new Scanner(System.in);
+        Scanner s1 = new Scanner(System.in);
+
+        PencarianBuku13 data = new PencarianBuku13();
+        int jumBuku = 5;
+
+        System.out.println("================================================");
+        System.out.println("Masukkan data Buku secara Urut dari kodeBuku terkecil : ");
+        
+        for (int i = 0; i < jumBuku; i++) {
+            System.out.println("Kode Buku \t: ");
+            int kodeBuku = s.nextInt();
+            System.out.println("Judul Buku \t: ");
+            String judulBuku = s1.nextLine();
+            System.out.println("Tahun terbit \t: ");
+            int tahunTerbit = s.nextInt();
+            System.out.println("Pengarang \t: ");
+            String pengarang = s1.nextLine();
+            System.out.println("Stock \t: ");
+            int stock = s.nextInt();
+
+            Buku13 m = new Buku13(kodeBuku, judulBuku, tahunTerbit, pengarang, stock);
+            data.tambah(m);
+        }
+
+        System.out.println("================================================");
+        System.out.println("Data keseluruhan Mahasiswa : ");
+        
+    }
 }
