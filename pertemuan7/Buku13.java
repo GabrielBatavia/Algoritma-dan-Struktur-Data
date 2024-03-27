f3f2c77797e5d2f5a041a1bd5d3b9a43335528a9
GabrielBatavia <gabrielbatavia7@gmail.com>
2024-03-27 11:15:46 +0700

    Fixxed Buku13

--- pertemuan7/Buku13.java
+++ pertemuan7/Buku13.java
@@ -6,11 +6,11 @@ public class Buku13 {
     int stock;
 
     public Buku13(int kodeBuku, String judulBuku, int tahunTerbit, String pengarang, int stock) {
-        kodeBuku = kodeBuku;
-        judulBuku = judulBuku;
-        tahunTerbit = tahunTerbit;
-        pengarang = pengarang;
-        stock = stock;
+        this.kodeBuku = kodeBuku;
+        this.judulBuku = judulBuku;
+        this.tahunTerbit = tahunTerbit;
+        this.pengarang = pengarang;
+        this.stock = stock;
     }
 
     public void tampilDataBuku() {
