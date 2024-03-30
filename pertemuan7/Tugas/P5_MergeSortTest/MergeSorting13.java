a920e28eb560646efd84f645d47d1fb98e6cbcc3
GabrielBatavia <gabrielbatavia7@gmail.com>
2024-03-30 22:44:44 +0700

    finish the code for MergeSorting

--- pertemuan7/Tugas/P5_MergeSortTest/MergeSorting13.java
+++ pertemuan7/Tugas/P5_MergeSortTest/MergeSorting13.java
@@ -40,5 +40,12 @@ public class MergeSorting13 {
         }
     }
 
+    public void printArray(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
 
 }
