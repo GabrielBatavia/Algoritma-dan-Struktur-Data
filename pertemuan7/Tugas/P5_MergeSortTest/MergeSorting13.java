f6144ff2459db8533fae4c316b8bfd2a1cbe952a
GabrielBatavia <gabrielbatavia7@gmail.com>
2024-03-30 22:38:25 +0700

    modify and write code in MergeSorting java

--- pertemuan7/Tugas/P5_MergeSortTest/MergeSorting13.java
+++ pertemuan7/Tugas/P5_MergeSortTest/MergeSorting13.java
@@ -3,7 +3,33 @@ package Tugas.P5_MergeSortTest;
 public class MergeSorting13 {
     
     public void mergeSort(int[] data) {
-        
+
     }
+
+    public void merge(int data[], int left, int middle, int right) {
+        int[] temp = new int[data.length];
+        for (int i = left; i <= right; i++) {
+            temp[i] = data[i];
+        }
+        int a = left;
+        int b = middle;
+        int c = left;
+
+        while (a <= middle && b <= right) {
+            if (temp[a] <= temp[b]) {
+                data[c] = temp[a];
+                a++;
+            } else {
+                data[c] = temp[b];
+                b++;
+            }
+            c++;
+        }
+        int s = middle -a;
+        for (int i = 0; i <= s; i++) {
+            data[c + i] = temp[a + i];
+        }
+    }
+    
 
 }
