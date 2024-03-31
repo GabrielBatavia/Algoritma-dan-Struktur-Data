b8d222b805826a217017f59735b03a938f27a4c6
GabrielBatavia <gabrielbatavia7@gmail.com>
2024-03-31 08:03:29 +0700

    add MergeSortMain13

---
+++ pertemuan7/Tugas/P5_MergeSortTest/MergeSortMain13.java
@@ -0,0 +1,14 @@
+package Tugas.P5_MergeSortTest;
+
+public class MergeSortMain13 {
+    public static void main(String[] args) {
+        int data[] = {10, 40, 30, 50, 70, 20, 100, 90};
+        System.out.println("Sorting dengan merge sort: ");
+        MergeSorting13 mSort = new MergeSorting13();
+        System.out.println("Data awal: ");
+        mSort.printArray(data);
+        mSort.mergeSort(data);
+        System.out.println("Setelah diurutkan: ");
+        mSort.printArray(data);
+    }
+}
