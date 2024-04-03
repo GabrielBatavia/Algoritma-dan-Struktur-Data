c28884de5269b26e70a257ae7723a2e6cfc31707
GabrielBatavia <gabrielbatavia7@gmail.com>
2024-04-03 10:49:01 +0700

    start pertemuan8 and add Barang13.java and write Barang13 class

---
+++ pertemuan8/Barang13.java
@@ -0,0 +1,13 @@
+
+public class Barang13 {
+
+    int kode;
+    String nama, kategori;
+
+    public Barang13(int kode, String nama, String kategori) {
+        this.kode = kode;
+        this.nama = nama;
+        this.kategori = kategori;
+    }
+        
+}
