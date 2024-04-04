4e69a840d25eac3938c173c9c24f361fb5ad795a
GabrielBatavia <gabrielbatavia7@gmail.com>
2024-04-05 00:12:21 +0700

    continue to write Postfix13 file

--- pertemuan8/Postfix13.java
+++ pertemuan8/Postfix13.java
@@ -2,6 +2,24 @@
  * Postfix13
  */
 public class Postfix13 {
+    int n, top;
+    String stack;
 
+    public Postfix13(int total) {
+        n = total;
+        top = -1;
+        stack = new char[n];
+        push('(');
+    }
     
+    public void push (char c) {
+        top++;
+        stack[top] = c;
+    }
+
+    public char pop() {
+        char item = stack[top];
+        top--;
+        return item;
+    }
 }
