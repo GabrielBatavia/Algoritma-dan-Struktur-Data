74d21307d247710d4517ab05fc4916058b77b70b
GabrielBatavia <gabrielbatavia7@gmail.com>
2024-04-27 12:26:36 +0700

    start make the main file and add menu method

---
+++ pertemuan10/QueueMain13.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class QueueMain13 {
+    
+    public static void menu() {
+        System.out.println("Masukkan operasi yang diinginkan:");
+        System.out.println("1. Engueue");
+        System.out.println("2. Dequeue");
+        System.out.println("3. Print");
+        System.out.println("4. Peek");
+        System.out.println("5. Clear");
+        System.out.println("-----------------");
+    }
+
+    public static void main(String[] args) {
+        
+        Scanner sc13 = new Scanner(System.in);
+
+    }
+
+}
