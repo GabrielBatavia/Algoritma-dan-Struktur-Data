63135459cfc252d4065cb07095b1ac7b6d06996f
GabrielBatavia <gabrielbatavia7@gmail.com>
2024-04-27 23:22:18 +0700

    finish Queue main

--- pertemuan10/QueueMain13.java
+++ pertemuan10/QueueMain13.java
@@ -16,6 +16,40 @@ public class QueueMain13 {
         
         Scanner sc13 = new Scanner(System.in);
 
+        System.out.print("Masukkan kapasitas queue: ");
+        int n = sc13.nextInt();
+
+        Queue13 Q = new Queue13(n);
+        int pilih;
+
+        do {
+            menu();
+            pilih = sc13.nextInt();
+            switch (pilih) {
+                case 1:
+                    System.out.print("Masukkan data baru: ");
+                    int dataMasuk = sc13.nextInt();
+                    Q.Enqueue(dataMasuk);
+                    break;
+                case 2:
+                    int datakeluar = Q.Dequeue();
+                    if (datakeluar != 0) {
+                        System.out.println("Data yang dikeluarkan: " + datakeluar);
+                    }
+                    break;
+                case 3:
+                    Q.print();
+                    break;
+                case 4:
+                    Q.peek();
+                    break;
+                case 5:
+                    Q.clear();
+                    break;
+            }
+        } while (pilih == 1 || pilih == 2 || pilih == 3 || pilih == 4 || pilih == 5);
+    
+
     }
 
 }
