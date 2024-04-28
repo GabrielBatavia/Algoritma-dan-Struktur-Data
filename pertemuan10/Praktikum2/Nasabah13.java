01ba2cc8f115ee4838bd1c0d1eab0f4f9d1c4633
GabrielBatavia <gabrielbatavia7@gmail.com>
2024-04-28 17:37:38 +0700

    make Nasabah class in praktikum2

---
+++ pertemuan10/Praktikum2/Nasabah13.java
@@ -0,0 +1,18 @@
+package Praktikum2;
+
+public class Nasabah13 {
+    private String norek;
+    private String nama;
+    private String alamat;
+    private int umur;
+    private double saldo;
+
+    public Nasabah13(String norek, String nama, String alamat, int umur, double saldo) {
+        this.norek = norek;
+        this.nama = nama;
+        this.alamat = alamat;
+        this.umur = umur;
+        this.saldo = saldo;
+    }
+
+}
