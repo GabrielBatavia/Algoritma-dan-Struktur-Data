beb214c3aeb9229d48743f0909aa1f4478c8bb7c
GabrielBatavia <gabrielbatavia7@gmail.com>
2024-04-29 18:11:16 +0700

    start tugas and add pembeli class and method

---
+++ pertemuan10/Tugas/Pembeli13.java
@@ -0,0 +1,12 @@
+package Tugas;
+
+public class Pembeli13 {
+    private String nama;
+    private int noHP;
+
+    public Pembeli13(String nama, int noHP) {
+        this.nama = nama;
+        this.noHP = noHP;
+    }
+
+}
